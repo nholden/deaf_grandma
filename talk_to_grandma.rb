67647a8ec0497be1f9c51a2a9c9576068637097b
Nick Holden <nholden@bu.edu>
2015-07-20 04:55:48 -0400

    Replace unneeded double quotes with single quotes

--- talk_to_grandma.rb
+++ talk_to_grandma.rb
@@ -1,13 +1,13 @@
 require './grandma.rb'
 
 grandma = Grandma.new
-puts "Grandma: HELLO, SONNY!"
+puts 'Grandma: HELLO, SONNY!'
 
 while true
-  print "You: "
+  print 'You: '
   response = grandma.respond_to(gets.chomp!)
   break if grandma.bye_count >= 3
-  puts "Grandma: " + response
+  puts 'Grandma: ' + response
 end
 
-puts "Grandma: GOODBYE, SONNY!"
+puts 'Grandma: GOODBYE, SONNY!'
