0e325157b7d83962cb049055d501d68e4486534e
Nick Holden <nholden@bu.edu>
2015-07-20 02:47:56 -0400

    Add test cases

--- spec/deaf_grandma_spec.rb
+++ spec/deaf_grandma_spec.rb
@@ -2,11 +2,27 @@ require_relative '../deaf_grandma.rb'
 
 describe Grandma do
   describe 'Grandma.respond_to' do
-    it "should respond correctly to speaking normally" do
-      expect(Grandma.respond_to('Hi, Grandma.')).to eql("HUH?  SPEAK UP, SONNY!")
+    not_shouted_messages = ["Hi, Grandma.", "Are you feeling 100%?"]
+    shouted_messages = not_shouted_messages.each { |message| message.upcase }
+
+    context 'when message is not shouted' do
+      not_shouted_messages.each do |message|
+        it "should respond correctly" do
+          expect(Grandma.respond_to(message)).to eql("HUH?  SPEAK UP, SONNY!")
+        end
+      end
     end
-    it "should respond correctly to shouting" do
-      expect(Grandma.respond_to('HI, GRANDMA.')[0..12]).to eql("NO, NOT SINCE")
+
+    context 'when message is shouted' do
+      shouted_messages.each do |message|
+        it "should respond correctly" do
+          expect(Grandma.respond_to(message)[0..12]).to eql("NO, NOT SINCE")
+        end
+        it "should respond with a year between 1930 and 1950" do
+          expect(Grandma.respond_to(message)[-4..-1].to_i).to be_between(1930, 1950)
+        end
+      end
     end
+
   end
 end
